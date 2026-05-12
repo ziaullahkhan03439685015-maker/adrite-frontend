1e9341a6848ec57b0acb166697f8bf9f89d73233
Ziaullahkhan03439685015-maker <ziaullah861168@gmail.com>
2026-05-12 21:04:52 +0500

    Add my files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Dashboard from './pages/client/Dashboard.jsx'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
   
-        
-        
+        <Dashboard/>
     </>
   )
 }
